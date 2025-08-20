681aa3212925993e3429f0bf5bbee85cf689678c
pranay bobade <pranaybobade84@gmail.com>
2025-08-20 13:13:12 +0530

    setuped basic app

---
+++ backend/src/app.js
@@ -0,0 +1,3 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+export const app = express();
---
+++ backend/src/index.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { app } from "./app.js";
+const PORT = process.env.PORT || 5000;
