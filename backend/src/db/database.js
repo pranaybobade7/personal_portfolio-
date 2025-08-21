eaef91d3f2abd924a988896e042e1a44e45d1dfd
pranay bobade <pranaybobade84@gmail.com>
2025-08-21 18:52:28 +0530

    eastablished server connection

---
+++ backend/src/db/database.js
@@ -0,0 +1,20 @@
+import { connect } from "mongoose";
+import { app } from "../app.js";
+
+export const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error("MONGODB_URI not found in env");
+    const connectionInstance = await connect(uri);
+    console.log(
+      `✅ Connected to MongoDB at host: ${connectionInstance.connection.host}`
+    );
+
+    app.on("error", (err) => {
+      console.error("Application error occurred:", err);
+    });
+  } catch (err) {
+    console.log("Failed to connect to MongoDB:", err.stack);
+    process.exit(1);
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,4 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { app } from "./app.js";
+import { connectDB } from "./db/database.js";
 const PORT = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Started on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Failed to start the server:", err);
+    process.exit(1);
+  });
